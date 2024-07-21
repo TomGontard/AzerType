7cd1bbfe4cd97edb2d211cffe4218ba064325f5f
Tom Gontard <tomgontard@protonmail.com>
2024-07-21 13:59:28 +0200

    Test du mot tapé par l'utilisateur

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-console.log("Hello World")
+const motApplication = "Test"
+let motUtilisateur = prompt("Entrez le mot : " + motApplication)
+
+if (motApplication === motUtilisateur) {
+    console.log("Le mot entré est correct.")
+} else {
+    console.log("Le mot entré est incorrect.")
+}
