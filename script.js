fc146cf865e801ca15e5de9494eb5730db7d1611
Tom Gontard <tomgontard@protonmail.com>
2024-07-21 15:54:11 +0200

    Amélioration boucle & intégration système de points (score)

--- script.js
+++ script.js
@@ -1,11 +1,20 @@
-const mots = ["Premier", "Deuxieme", "Troisieme"]
+const mots = ["Cachalot", "Pétunia", "Serviette"]
 
+let score = 0
 
 for (let mot of mots) {
     let motUtilisateur = prompt("Entrez le mot : " + mot)
     if (motUtilisateur === mot) {
         console.log("Le mot entré est correct.")
+        score += 1
     } else {
         console.log("Le mot entré est incorrect.")
     }
+    if (score > 1) {
+        console.log("Vous avez " + score + " points.")
+    } else {
+        console.log("Vous avez " + score + " point.")
+    }
 }
+
+console.log("Vous avez eu " + score + " points au total.")
