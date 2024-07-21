e8e655462f944d7366b12f7065c3b486728e11c8
Tom Gontard <tomgontard@protonmail.com>
2024-07-21 16:07:48 +0200

    Ajout d'une liste de phrases et d'un choix pour l'utilisateur de choisir entre les deux (mots ou phrases)

--- script.js
+++ script.js
@@ -1,8 +1,23 @@
-const mots = ["Cachalot", "Pétunia", "Serviette"]
+const listeMots = ["Cachalot", "Pétunia", "Serviette"]
+const listePhrases = ["Pas de panique !", "La vie, l'univers et le reste", "Merci pour le poisson"]
 
 let score = 0
+let reponse = ""
+let liste = []
 
-for (let mot of mots) {
+while (reponse !== "mots" && reponse !== "phrases") {
+    reponse = prompt("Voulez-vous jouer avec des mots ou des phrases ?")
+}
+
+console.log("Vous avez choisi la liste de " + reponse)
+
+if (reponse === "mots") {
+    liste = listeMots
+} else {
+    liste = listePhrases
+}
+
+for (let mot of liste) {
     let motUtilisateur = prompt("Entrez le mot : " + mot)
     if (motUtilisateur === mot) {
         console.log("Le mot entré est correct.")
