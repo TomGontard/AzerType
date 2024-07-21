d944ca851facfce6ce129fcd667762c951412f02
Tom Gontard <tomgontard@protonmail.com>
2024-07-21 18:15:47 +0200

    Ajout d'une fonction lancerJeu

--- fonctions.js
+++ fonctions.js
@@ -31,3 +31,24 @@ function lancerBoucleDeJeu(liste, nombreQuestions) {
     }
     return score
 }
+
+function lancerJeu() {
+    let score = 0
+    let nombreQuestions = 0
+    let reponse = ""
+    let liste = []
+
+    reponse = choisirPhrasesOuMots()
+
+    console.log("Vous avez choisi la liste de " + reponse)
+
+    if (reponse === "mots") {
+        liste = listeMots
+    } else {
+        liste = listePhrases
+    }
+
+    score = lancerBoucleDeJeu(liste, nombreQuestions)
+
+    afficherResultat(score, liste.length)
+}
