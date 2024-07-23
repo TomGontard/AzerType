01bfdea78bbfaf07fe6aa41c33fb51f99645419c
Tom Gontard <tomgontard@protonmail.com>
2024-07-23 18:13:10 +0200

    Affichage graphique des mots à écrire et vidage du champ de saisie

--- scripts/script.js
+++ scripts/script.js
@@ -15,6 +15,15 @@ function afficherResultat(score, nbMotsProposes) {
 }
 
 /**
+ * Cette fonction affiche dans l'encadré prévu à cet effet le mot à écrire
+ */
+function afficherProposition(mot) { 
+    let zoneProposition = document.querySelector(".zoneProposition")
+    console.log(zoneProposition)
+    zoneProposition.textContent = mot
+}
+
+/**
  * Cette fonction lance le jeu. 
  * Elle demande à l'utilisateur de choisir entre "mots" et "phrases" et lance la boucle de jeu correspondante
  */
@@ -22,12 +31,25 @@ function lancerJeu() {
     // Initialisations
     let score = 0
     let nbMotsProposes = 0
+    let compteurMots = 0
     let inputEcriture = document.getElementById("inputEcriture")
-
     let btnValiderMot = document.getElementById("btnValiderMot")
+
+    afficherProposition(listeMots[compteurMots])
+
     btnValiderMot.addEventListener("click", () => {
         console.log(inputEcriture.value)
+        console.log(listeMots[compteurMots])
+        compteurMots += 1
+        if (listeMots[compteurMots] === undefined) { 
+            afficherProposition("Le jeu est fini")
+            btnValiderMot.disabled = true
+        } else {
+            afficherProposition(listeMots[compteurMots])
+        }
+        inputEcriture.value = ""
     });
+
 
     
 
