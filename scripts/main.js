a9fda105e91aa615829243081bbffb452ed9e3f8
Tom Gontard <tomgontard@protonmail.com>
2024-07-23 12:17:43 +0200

    Récupération d'éléments de la page via JavaScript

--- scripts/main.js
+++ scripts/main.js
@@ -4,4 +4,19 @@
  * 
  *********************************************************************************/
 
+let inputEcriture = document.getElementById("inputEcriture")
+console.log(inputEcriture)
+
+let btnValiderMot = document.getElementById("btnValiderMot")
+console.log(btnValiderMot)
+
+let zoneProposition = document.querySelector(".zoneProposition")
+console.log(zoneProposition)
+
+let zoneScore = document.querySelector(".zoneScore span")
+console.log(zoneScore)
+
+let optionSource = document.querySelectorAll(".optionSource input")
+console.log(optionSource)
+
 lancerJeu()
